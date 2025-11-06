b3644079a7b97252e4dcc48f3b7910f179b99aff
Nicolas Delbovier <nicolas.delbovier@acsone.eu>
2025-11-06 11:37:48 +0100

    [ADD] helpdesk_mgmt_purchase: links purchase orders and helpdesk tickets.

---
+++ helpdesk_mgmt_purchase/models/helpdesk_ticket.py
@@ -0,0 +1,35 @@
+# Copyright 2017 Camptocamp SA
+# Copyright 2023 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+
+
+class HelpdeskTicket(models.Model):
+    _inherit = "helpdesk.ticket"
+
+    purchase_order_ids = fields.Many2many("purchase.order")
+    po_count = fields.Integer(
+        string="Purchase Order Count", compute="_compute_po_count", store=True
+    )
+
+    @api.depends("purchase_order_ids")
+    def _compute_po_count(self):
+        for ticket in self:
+            ticket.po_count = len(ticket.purchase_order_ids)
+
+    def action_view_purchase_orders(self):
+        """Returns action to view purchase orders related to this ticket."""
+        action = {
+            "name": "Purchase Orders",
+            "type": "ir.actions.act_window",
+            "res_model": "purchase.order",
+            "view_mode": "tree,form",
+            "target": "current",
+        }
+        action["domain"] = [("ticket_ids", "in", [self.id])]
+        action["context"] = {
+            "default_ticket_ids": [(4, [self.id])],
+            "default_partner_id": self.partner_id.id,
+        }
+        return action
---
+++ helpdesk_mgmt_purchase/tests/test_helpdesk_ticket_purchase.py
@@ -0,0 +1,57 @@
+# Copyright 2025 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import Command
+from odoo.tests.common import TransactionCase
+
+
+class TestHelpdeskTicketPurchase(TransactionCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.partner = cls.env["res.partner"].create(
+            {"name": "Test Partner", "email": "testpartner@example.com"}
+        )
+        cls.ticket = cls.env["helpdesk.ticket"].create(
+            {
+                "name": "Test Helpdesk Ticket",
+                "partner_id": cls.partner.id,
+                "description": "Test Helpdesk Ticket",
+            }
+        )
+        cls.purchase_order_1 = cls.env["purchase.order"].create(
+            {
+                "partner_id": cls.partner.id,
+                "ticket_ids": [Command.set([cls.ticket.id])],
+            }
+        )
+        cls.purchase_order_2 = cls.env["purchase.order"].create(
+            {
+                "partner_id": cls.partner.id,
+                "ticket_ids": [Command.set([cls.ticket.id])],
+            }
+        )
+
+    def test_purchase_orders_associated_with_ticket(self):
+        # Verify that a Helpdesk ticket has multiple purchase orders associated with it.
+        self.assertEqual(len(self.ticket.purchase_order_ids), 2)
+        self.assertIn(self.purchase_order_1, self.ticket.purchase_order_ids)
+        self.assertIn(self.purchase_order_2, self.ticket.purchase_order_ids)
+
+    def test_partner_association_in_purchase_order(self):
+        # Verify that a purchase order is associated with the correct ticket partner.
+        self.assertEqual(self.purchase_order_1.partner_id, self.partner)
+        self.assertEqual(self.purchase_order_2.partner_id, self.partner)
+
+    def test_smartbutton_sale_order_count(self):
+        # Check the purchase order counter in the smartbutton of the ticket.
+        self.ticket._compute_po_count()
+        self.assertEqual(self.ticket.po_count, 2)
+
+    def test_action_view_purchase_orders(self):
+        # Verify that the smartbutton action displays the associated orders correctly.
+        action = self.ticket.action_view_purchase_orders()
+        self.assertEqual(action["domain"], [("ticket_ids", "in", [self.ticket.id])])
+        self.assertEqual(
+            action["context"]["default_ticket_ids"], [(4, [self.ticket.id])]
+        )
