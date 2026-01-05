b4735fdaf18b19074bdf38d4c03048582fa8f1aa
Nicolas Delbovier <nicolas.delbovier@acsone.eu>
2026-01-05 10:54:12 +0100

    [ADD] helpesk_mgmt_stock_grn: links GRN to tickets

---
+++ helpdesk_mgmt_stock_grn/tests/test_helpdesk_mgmt_stock_grn.py
@@ -0,0 +1,13 @@
+# Copyright 2026 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo.tests.common import TransactionCase
+
+
+class TestHelpdeskMgmtStockGrn(TransactionCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+    def test_1(self):
+        pass
